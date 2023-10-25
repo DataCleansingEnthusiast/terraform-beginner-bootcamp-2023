d4d80f7bb2b9393e2c420c4f4ef693d9fda6ae18
roopish <rmurthy3554@gmail.com>
2023-10-25 09:49:48 -0700

    #52 correct the towns, update journal (#53)

--- main.tf
+++ main.tf
@@ -21,22 +21,39 @@ provider "terratowns" {
   token=var.terratowns_access_token
 }
 
-module "home_climatechange_hosting" {
+# module "home_climatechange_hosting" {
+#   source = "./modules/terrahome_aws"
+#   user_uuid = var.teacherseat_user_uuid
+#   public_path     = var.climatechange.public_path
+#   content_version = var.climatechange.content_version
+# }
+
+# resource "terratowns_home" "climatechange" {
+#   name        = "Climate change "
+#   description = <<DESCRIPTION
+#   roopish, a technologist/mom/global citizen is trying to figure out what she can do about climate change.
+#   There are things each of us can do to minimize our environmental impact. 
+#   DESCRIPTION
+#   domain_name = module.home_climatechange_hosting.domain_name
+#   town            = "missingo"
+#   content_version = var.climatechange.content_version
+# }
+module "home_music_hosting" {
   source = "./modules/terrahome_aws"
   user_uuid = var.teacherseat_user_uuid
-  public_path     = var.climatechange.public_path
-  content_version = var.climatechange.content_version
+  public_path     = var.music.public_path
+  content_version = var.music.content_version
 }
 
-resource "terratowns_home" "climatechange" {
-  name        = "Climate change "
+resource "terratowns_home" "music" {
+  name        = "Music"
   description = <<DESCRIPTION
-  roopish, a technologist/mom/global citizen is trying to figure out what she can do about climate change.
-  There are things each of us can do to minimize our environmental impact. 
+  Indian fusion music is a captivating blend of traditional Indian musical elements, such as classical instruments and melodies, with Western and global music genres, resulting in a harmonious fusion of diverse musical traditions. 
+  This genre often celebrates the cross-cultural exchange of musical ideas, creating a rich tapestry of sound that resonates with audiences worldwide. "Shakti" is one such fusion band that started in 1973.
   DESCRIPTION
-  domain_name = module.home_climatechange_hosting.domain_name
-  town            = "missingo"
-  content_version = var.climatechange.content_version
+  domain_name = module.home_music_hosting.domain_name
+  town            = "melomaniac-mansion"
+  content_version = var.music.content_version
 }
 
 #2
@@ -46,7 +63,6 @@ module "home_travel_hosting" {
   public_path     = var.travel.public_path
   content_version = var.travel.content_version
 }
-
 
 #2
 resource "terratowns_home" "travel" {
